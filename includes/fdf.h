1fe36f4271bbb879b87401d490241fcef0d48230
Benson Lee <blee@e1z2r8p37.42.us.org>
2018-09-11 19:23:57 -0700

    Still working out the math

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: blee <blee@student.42.us.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/01 18:36:43 by blee              #+#    #+#             */
-/*   Updated: 2018/09/04 17:29:33 by blee             ###   ########.fr       */
+/*   Updated: 2018/09/11 19:06:53 by blee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,20 @@ typedef struct			s_map_arr
 	int					y_size;
 }						t_map_arr;
 
+typedef	struct			s_pxls
+{
+	int					x;
+	int					y;
+}						t_pxls;	
+
 typedef struct			s_data
 {
-	int					**map;
-	int					size_x;
-	int					size_y;
+	t_map_arr			**map;
+	t_pxls				**pxls;
+	int					x_size;
+	int					y_size;
+	int					zoom;
+	int					height;
 }						t_data;
 
 t_map_lst				*read_map(char *file);
